dcd8d56f2c5444eaeb1d9e76872956b9ca0c8bfd
qnslgksk <hkim5579@gmail.com>
2022-11-24 17:30:07 +0900

    tuenitueni update0

--- tuenirueni/js/common.js
+++ tuenirueni/js/common.js
@@ -2,4 +2,30 @@ $(function () {
     const swiper = new Swiper('.visual', {
         loop: true,
     });
+    swiper.on('slideChange', function () {
+        var idx = swiper.realIndex;
+        if(idx == '0'){
+            color('#F17785')
+            // $('.gnb li').each(function(){
+            //     if($(this).hasClass('active')){
+            //         $(this).children('a').css('color', '#F17785')
+            //     }
+            // })
+        }else if(idx == '1'){
+            color('#ABCD05')
+            // $('.gnb li').each(function(){
+            //     if($(this).hasClass('active')){
+            //         $(this).children('a').css('color', '#ABCD05')
+            //     }
+            // })
+        }
+    });
+
+    function color(co){
+        $('.gnb li').each(function(){
+            if($(this).hasClass('active')){
+                $(this).children('a').css('color', co)
+            }
+        })
+    }
 })
