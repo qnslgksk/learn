d560232b076071c2e180e5759ca5e3b025230862
qnslgksk26 <qnslgksk26@gmail.com>
2024-11-05 12:22:25 +0900

    tueniueni typeA complete

--- tuenirueni/js/common.js
+++ tuenirueni/js/common.js
@@ -1,4 +1,22 @@
-// Swiper
+// program popup
+$(document).ready(function(){
+    $('#btnProgramView').click(function(){
+        $('.program_pop_dim').addClass('open');
+    })
+    $('#btnProgramPopClose').click(function(){
+        $('.program_pop_dim').removeClass('open');
+    })
+});
+
+// benefit popup
+$(document).ready(function(){
+    $('.benefit_pop_dim').addClass('show');
+    $('#btnBenefitPopClose').click(function(){
+        $('.benefit_pop_dim').removeClass('show');
+    })
+});
+
+// visual Swiper
 $(function () {
     const swiper = new Swiper('.visual', {
         loop: true,
@@ -32,4 +50,29 @@ $(function () {
             }
         })
     }
-})
+    const swiper1 = new Swiper('.letter', {
+        slidesPerView: "auto",
+        spaceBetween: 15,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
+    const swiper2 = new Swiper('.program', {
+        slidesPerView: "auto",
+        spaceBetween: 15,
+        breakpoints:{
+            580:{
+                slidesPerView:3,
+            },
+            960:{
+                slidesPerView:4,
+            },
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+});
